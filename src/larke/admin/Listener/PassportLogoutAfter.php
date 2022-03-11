90ee4782d67a0192c51499d161e5d2b5a8f26153
lishuang <leon.li01@tron.network>
2022-03-11 17:33:08 +0800

    清空缓存

--- src/larke/admin/Listener/PassportLogoutAfter.php
+++ src/larke/admin/Listener/PassportLogoutAfter.php
@@ -4,6 +4,7 @@ declare (strict_types = 1);
 
 namespace Larke\Admin\Listener;
 
+use Illuminate\Support\Facades\Cache;
 use Larke\Admin\Event;
 use Larke\Admin\Model\Admin as AdminModel;
 
@@ -24,5 +25,8 @@ class PassportLogoutAfter
             'refresh_time' => time(), 
             'refresh_ip' => request()->ip(),
         ]);
+
+      //晴空选中游戏大区缓存
+      Cache::forget($adminid.env('CACHE_USER_KEY'));
     }
 }
