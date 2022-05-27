f13650d3e35b5aeb09763ce910620c5d96a305a4
lishuang <leon.li01@tron.network>
2022-05-28 01:08:14 +0800

    时间格式化

--- src/larke/admin/Model/Base.php
+++ src/larke/admin/Model/Base.php
@@ -4,6 +4,7 @@ declare (strict_types = 1);
 
 namespace Larke\Admin\Model;
 
+use DateTimeInterface;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Eloquent\Model;
@@ -169,5 +170,18 @@ class Base extends Model
     {
         return ($this->status == 1);
     }
-    
+
+
+    /**
+     * 为数组 / JSON序列化准备一个日期
+     *
+     * @param DateTimeInterface $date
+     * @return string
+     */
+    protected function serializeDate(DateTimeInterface $date): string
+    {
+        return $date->format('Y-m-d H:i:s');
+    }
+
+
 }
